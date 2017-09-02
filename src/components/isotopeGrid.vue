2b682ae095104cbb9ba4b344e58432b2d97fa9e8
Jonathan Alumbaugh <jonathan.alumbaugh@gmail.com>
2017-09-01 20:44:54 -0400

    Just a lil change.
    Former-commit-id: 1182f4946c357ce71db7fcd1443600ef1382bfa8

---
+++ src/components/isotopeGrid.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <div class='row mb-160'>
+    <div class='offset-md-1 col-sm-9'>
+      <div id='vue-grid-items' class='grid'>
+        <div class='grid-sizer'></div>
+        <slot></slot>
+        <!-- <isotope-item v-for="item in items">{{ name.category }}</isotope-item> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import isotopeItem from '~/components/isotopeItem.vue'
+export default {
+  name: 'isotopeGrid',
+  components: {
+    isotopeItem
+  }
+
+  // Ideally, everything in index.vue will eventually
+  // propogate this list which will then feed into
+  // the v-for property of <isotope-item>
+  // data:
+  // {
+  //   items: [
+  //     {
+  //       name: 'senior project',
+  //       category: 'design'
+  //   },
+  //     {
+  //       name: 'patterns',
+  //       category: 'photography'
+  //   },
+  //     {
+  //       name: 'exhibition catalog',
+  //       category: 'design'
+  //   },
+  //     {
+  //       name: 'human interest',
+  //       category: 'photojournalism'
+  //   },
+  //     {
+  //       name: 'STING vodka branding',
+  //       category: 'design'
+  //   },
+  //     {
+  //       name: 'instagram',
+  //       category: 'photography'
+  //   },
+  //     {
+  //       name: 'Benoit golf tournament',
+  //       category: 'design'
+  //   } ]
+  // }
+}
+</script>
---
+++ src/components/projectImgC.vue
@@ -0,0 +1,81 @@
+<template lang="html">
+  <div class="row">
+       <div class="offset-md-1 col-md-9">
+            <div class="row">
+                 <div class="col-sm-4 box-shadow mb-40">
+                 <a data-fancybox="gallery" :href="imgPath1">
+                      <img :src="imgPath1" :alt="altText1" class="img-fluid hvrbox-layer_bottom grayscale">
+                 </a>
+                 </div>
+                 <div class="col-sm-4 box-shadow mb-40">
+                 <a data-fancybox="gallery" :href="imgPath2">
+                      <img :src="imgPath2" :alt="altText2" class="img-fluid hvrbox-layer_bottom grayscale">
+                 </a>
+                 </div>
+                 <div class="col-sm-4 box-shadow mb-40">
+                 <a data-fancybox="gallery" :href="imgPath3">
+                      <img :src="imgPath3" :alt="altText3" class="img-fluid hvrbox-layer_bottom grayscale">
+                 </a>
+                 </div>
+            </div>
+       </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'projectImgC',
+  props: {
+    name: {
+      required: true
+    },
+    category: {
+      required: true
+    },
+    imgNumber1: {
+      required: true
+    },
+    imgNumber2: {
+      required: true
+    },
+    imgNumber3: {
+      required: true
+    }
+  },
+
+  computed: {
+    altText1: function () {
+      return (
+        this.name + ' in ' + this.category + ', image ' + this.imgNumber1
+      )
+    },
+
+    altText2: function () {
+      return (
+        this.name + ' in ' + this.category + ', image ' + this.imgNumber2
+      )
+    },
+
+    altText3: function () {
+      return (
+        this.name + ' in ' + this.category + ', image ' + this.imgNumber3
+      )
+    },
+
+    imgPath1: function () {
+      return 'img/' + this.category + '/' + this.name.toLowerCase().replace( / /g, '-' ) + '/' + this.name.toLowerCase().replace( / /g, '-' ) + this.imgNumber1 + '.jpg'
+    },
+
+    imgPath2: function () {
+      return 'img/' + this.category + '/' + this.name.toLowerCase().replace( / /g, '-' ) + '/' + this.name.toLowerCase().replace( / /g, '-' ) + this.imgNumber2 + '.jpg'
+    },
+
+    imgPath3: function () {
+      return 'img/' + this.category + '/' + this.name.toLowerCase().replace( / /g, '-' ) + '/' + this.name.toLowerCase().replace( / /g, '-' ) + this.imgNumber3 + '.jpg'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
