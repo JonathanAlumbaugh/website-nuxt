8c91f31429d2dbb966b4941eadc97e3a209ec4ef
Jonathan Alumbaugh <jonathan.alumbaugh@gmail.com>
2017-09-01 20:44:54 -0400

    Added Archivo Bold and Open Sans Light/Regular for testing.
    Former-commit-id: ae77d5f3c09881e460667a80a30d18a38a3f7179

---
+++ src/components/mainHeader.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="row pt-40 pb-10">
+    <div class="col-sm-11 align-middle">
+      <h1 class="text-center align-middle">Jonathan Alumbaugh</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default
+{
+  name: 'mainHeader'
+}
+</script>
---
+++ src/pages/textile-patterns.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+  <section id="textile-patterns">
+    <!-- 01 -->
+    <t>as part of a textile class
+    </t>
+    <tt>Jonathan created a bunch of patterns for textiles. He developed the patterns, mixed inks, dyed fabrics, and printed everything by hand.
+    </tt>
+
+    <!-- 02 -->
+    <img-one name="textile-patterns" category="design" img-number="04"></img-one>
+    <img-two name="textile-patterns" category="design" img-number1="06" img-number2="07"></img-two>
+    <tt>The very slight lines were difficult to clean once the screen had been exposed. Dye-based ink was used as it could be diluted to reduce clogging the screen, without bleeding.</tt>
+    <img-one name="textile-patterns" category="design" img-number="12"></img-one>
+    <img-two name="textile-patterns" category="design" img-number1="11" img-number2="13"></img-two>
+    <tt>It's not weed. It could be, but it was intended to be Japanese maple leaves. This color combination was printed second to the following combination, but is favored. The background is dyed a pthalo blue that carries slight variation, while the maple leaves are metallic ink.</tt>
+    <img-one name="textile-patterns" category="design" img-number="01"></img-one>
+    <img-two name="textile-patterns" category="design" img-number1="02" img-number2="03"></img-two>
+    <tt>Still not weed.</tt>
+    <img-one name="textile-patterns" category="design" img-number="08"></img-one>
+    <img-two name="textile-patterns" category="design" img-number1="09" img-number2="10"></img-two>
+    <tt>This is less a pattern than a heavily textured print. Most of the fabric was monoprinted using plastic sheeting, while the cirlce was blocked out and printed afterwards.</tt>
+
+    <home-btn></home-btn>
+  </section>
+</template>
+
+<script>
+import t from '~/components/textH2.vue'
+import tt from '~/components/textH3.vue'
+import imgOne from '~/components/projectImgA.vue'
+import imgTwo from '~/components/projectImgB.vue'
+import imgThree from '~/components/projectImgC.vue'
+import homeBtn from '~/components/homeBtn.vue'
+export default {
+  // Head content all up in here
+  name: 'textile-patterns',
+  data: function () {
+    return {
+      title: 'textile patterns'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Jonathan developed and printed graphic patterns for textiles'
+        }
+      ]
+    }
+  },
+  // /Head content
+  components: {
+    t,
+    tt,
+    imgOne,
+    imgTwo,
+    imgThree,
+    homeBtn
+  }
+}
+</script>
