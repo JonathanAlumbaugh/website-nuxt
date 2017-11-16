02e9f7ffd0a07699baf9362b80bf7dc3d95e09c0
JonathanAlumbaugh <jonathan.alumbaugh@gmail.com>
2017-11-16 02:26:40 -0500

    Making about responsive.

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,18 +2,18 @@
   <section id="about">
 
     <!-- abt-int -->
-    <t class="abt-int-1">I'm Jonathan Alumbaugh,</t>
+    <t class="abt-int int-1">I'm Jonathan Alumbaugh,</t>
 
-    <tt class="abt-body-1">a New England native with an education in photojournalism and graphic design. I'm interested in ways of promoting community through design. I hope to observe, tell the story of, and refine the interaction between people and design, and between people and people.<br />
+    <tt class="abt-body body-1">a New England native with an education in photojournalism and graphic design. I'm interested in ways of promoting community through design. I hope to observe, tell the story of, and refine the interaction between people and design, and between people and people.<br />
 
     <br />Someday I'll find myself halfway across the world (likely without a jacket or a clean pair of socks) in pursuit of a design challenge. In the meantime, I seek a design challenge on the homefront, in the Rhode Island/Southcoast/Boston area.
   </tt>
 
 
   <!-- Education -->
-  <t class="abt-int-2">My education spans 5 years and 2 schools</t>
+  <t class="abt-int int-2">My education spans 5 years and 2 schools</t>
 
-  <div class="abt-body-2 box-shadow">
+  <div class="abt-body body-2 box-shadow">
     <h3>BFA graphic design</h3>
     <ul class="school">
       <li>UMASS Dartmouth 2017</li>
@@ -25,7 +25,7 @@
     </ul>
   </div>
 
-  <div class="abt-body-2 box-shadow">
+  <div class="abt-body body-2 box-shadow">
     <h3>AA graphic design</h3>
     <ul class="school">
       <li>Quinebaug Valley CC 2014</li>
@@ -40,10 +40,10 @@
 
 
   <!-- Experience -->
-  <t class="abt-int-3">my work experience includes titles like</t>
+  <t class="abt-int int-3">my work experience includes titles like</t>
 
   <blk-lg
-    class="abt-body-3"
+    class="abt-body body-3"
     category="designer"
     title="In house designer"
     subtitle="Minuteman Press Dartmouth, May 2017-present"
@@ -51,7 +51,7 @@
   </blk-lg>
 
   <blk-lg
-    class="abt-body-3"
+    class="abt-body body-3"
     category="photographer"
     title="Staff photographer"
     subtitle="Horizons Inc, 2014-2016"
@@ -59,7 +59,7 @@
   </blk-lg>
 
   <blk-lg
-    class="abt-body-3"
+    class="abt-body body-3"
     category="photojournalist"
     title="Freelance photographer"
     subtitle="The Chronicle Newspaper, 2012-2015"
@@ -74,7 +74,7 @@
   </blk-lg> -->
 
   <blk-lg
-    class="abt-body-3"
+    class="abt-body body-3"
     title="Volunteer"
     subtitle="Windham Area Interfaith Ministry, Jun-Oct 2013"
     description="Volunteered over 200 hours at local organization that supports underprivileged families">
@@ -82,10 +82,10 @@
 
 
   <!-- Awards -->
-  <t class="abt-int-4">I received an award for</t>
+  <t class="abt-int int-4">I received an award for</t>
 
   <blk-lg
-    class="abt-body-4"
+    class="abt-body body-4"
     category="photojournalist"
     title="Excellence in journalism"
     subtitle="Society of Professional Journalists, 2014"
@@ -93,47 +93,47 @@
 
 
   <!-- Programs -->
-  <t class="abt-int-5">and am a pro with industry standard programs like</t>
+  <t class="abt-int int-5">and am a pro with industry standard programs like</t>
 
-  <blk-sm class="abt-body-5" title="Il" description="Adobe Illustrator"/>
+  <blk-sm class="abt-body body-5" title="Il" description="Adobe Illustrator"/>
 
-  <blk-sm class="abt-body-5" title="Id" description="Adobe Indesign"/>
+  <blk-sm class="abt-body body-5" title="Id" description="Adobe Indesign"/>
 
-  <blk-sm class="abt-body-5" title="Lr" description="Adobe Lightroom"/>
+  <blk-sm class="abt-body body-5" title="Lr" description="Adobe Lightroom"/>
 
-  <blk-sm class="abt-body-5" title="Ps" description="Adobe Photoshop"/>
+  <blk-sm class="abt-body body-5" title="Ps" description="Adobe Photoshop"/>
 
 
   <!-- More -->
-  <t class="abt-int-6">plus I can work with</t>
+  <t class="abt-int int-6">plus I can work with</t>
 
-  <blk-sm class="abt-body-6" title="Ae" description="Adobe After Effects"/>
+  <blk-sm class="abt-body body-6" title="Ae" description="Adobe After Effects"/>
 
-  <blk-sm class="abt-body-6" title="At" description="Atom Text Editor"/>
+  <blk-sm class="abt-body body-6" title="At" description="Atom Text Editor"/>
 
-  <blk-sm class="abt-body-6" title="Au" description="Adobe Audition"/>
+  <blk-sm class="abt-body body-6" title="Au" description="Adobe Audition"/>
 
-  <blk-sm class="abt-body-6" title="Bs" description="Bootstrap Framework"/>
+  <blk-sm class="abt-body body-6" title="Bs" description="Bootstrap Framework"/>
 
-  <blk-sm class="abt-body-7" title="C4D" description="Cinema 4D"/>
+  <blk-sm class="abt-body body-7" title="C4D" description="Cinema 4D"/>
 
-  <blk-sm class="abt-body-7" title="CSS" description="Cascading Style Sheets"/>
+  <blk-sm class="abt-body body-7" title="CSS" description="Cascading Style Sheets"/>
 
-  <blk-sm class="abt-body-7" title="HTML" description="Hooking up Those Massive Logos"/>
+  <blk-sm class="abt-body body-7" title="HTML" description="Hooking up Those Massive Logos"/>
 
-  <blk-sm class="abt-body-7" title="Pr" description="Adobe Premiere Pro"/>
+  <blk-sm class="abt-body body-7" title="Pr" description="Adobe Premiere Pro"/>
 
 
   <!-- Meta -->
-  <t class="abt-int-8">I built this website using</t>
+  <t class="abt-int int-8">I built this website using</t>
 
-  <blk-sm class="abt-body-8" title="At" description="Atom Text Editor"/>
+  <blk-sm class="abt-body body-8" title="At" description="Atom Text Editor"/>
 
-  <blk-sm class="abt-body-8" title="HTML+CSS" description="HyperText Markup Language + Cascading Style Sheets"/>
+  <blk-sm class="abt-body body-8" title="HTML+CSS" description="HyperText Markup Language + Cascading Style Sheets"/>
 
-  <blk-sm class="abt-body-8" title="Is" description="Isotope.js"/>
+  <blk-sm class="abt-body body-8" title="Is" description="Isotope.js"/>
 
-  <blk-sm class="abt-body-8" title="Nuxt.js" description="Vue.js, but SSR!"/>
+  <blk-sm class="abt-body body-8" title="Nuxt.js" description="Vue.js, but SSR!"/>
 
   </section>
 </template>
@@ -144,15 +144,18 @@ import tt from '~/components/textH3.vue'
 import blkLg from '~/components/blockLarge.vue'
 import blkSm from '~/components/blockSmall.vue'
 import homeBtn from '~/components/homeBtn.vue'
-export default {
+export default
+{
   // Head content all up in here
   name: 'about',
-  data: function () {
+  data: function ()
+  {
     return {
       title: 'About'
     }
   },
-  head() {
+  head()
+  {
     return {
       title: this.title,
       meta: [
@@ -165,7 +168,8 @@ export default {
     }
   },
   // /Head content
-  components: {
+  components:
+  {
     t,
     tt,
     blkLg,
