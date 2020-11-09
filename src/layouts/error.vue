3b7aaa6725f371c020b061aef855061128c130a8
Jonathan Alumbaugh <jalumbaugh@duffyshanley.com>
2020-11-08 22:28:05 -0500

    Remove unused components and methods

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -5,11 +5,6 @@
     enter-active-class="animated fadeIn"
     leave-active-class="animated fadeOut"
   >
-    <!-- Not sure about this...  -->
-    <!-- <div class="container"> -->
-    <!-- <main-header/>
-    <main-nav/> -->
-    <!-- ...through this. -->
     <section class="error">
       <t v-if="error.statusCode === 404">I was fiddling around and</t>
 
@@ -31,28 +26,16 @@
         <pre>{{ error.locations }}</pre>
       </tt>
     </section>
-    <!-- <main-footer/> -->
-    <!-- </div> -->
   </transition>
 </template>
 
 <script>
-// import mainHeader from '~/components/mainHeader.vue'
-// import mainNav from '~/components/mainNav.vue'
-// import mainFooter from '~/components/mainFooter.vue'
 import t from '~/components/textH2.vue'
 import tt from '~/components/tt.vue'
 export default {
   props: ['error'],
-  methods: {
-    show() {
-      this.$modal.show('contact-modal')
-    },
-  },
+
   components: {
-    // mainHeader,
-    // mainNav,
-    // mainFooter,
     t,
     tt,
   },
